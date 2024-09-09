ae34db4fa44a9bf8dd7eb563fba821e89dde86c6
renaruna <79509944+renaruna@users.noreply.github.com>
2024-09-09 18:32:55 +0900

    ちょい間違えたけどjs自体はいけそうだったので、微修正

--- test_character.js
+++ test_character.js
@@ -1,26 +1,39 @@
 fetch('https://script.google.com/macros/s/AKfycbyjMTeRaNtT2HKEKkYdilkQg3RYP9JOlDJ8s27e7HK6NeRv67cwv03RLVyWRwDC-pN03A/exec?member=' + 'manager')
     .then(response => response.json())
     .then(function (data) {
-        const nameElement = document.getElementById('charaName');
+        const charaNameElement = document.getElementById('charaName');
         const hiranameElement = document.getElementById('hiraname');
         const iconElement = document.getElementById('icon');
         const iconMakerElement = document.getElementById('iconMaker');
         const introductionElement = document.getElementById('introduction');
+
         const ageElement = document.getElementById('age');
         const sexElement = document.getElementById('sex');
         const heightElement = document.getElementById('height');
         const jobElement = document.getElementById('job');
         const birthdayElement = document.getElementById('birthday');
-        const colorElement = document.getElementById('charaColor');
-        const makerElement = document.getElementById('maker');
+        const charaColorElement = document.getElementById('charaColor');
 
         const scenarioListElement = document.getElementById('scenarioList');
         const charaLinkElement = document.getElementById('charaLink');
         const freeCommentElement = document.getElementById('freeComment');
 
-        nameElement.textContent = data[charaID].name;
-        hiranameElement.textContent = data[charaID].hiraname;
+
+        charaNameElement.textContent = data[charaID - 1].charaName;
+        hiranameElement.textContent = data[charaID - 1].hiraname;
+        iconElement.setAttribute("src", data[charaID - 1].icon);
+        iconMakerElement.textContent = data[charaID - 1].iconMaker;
+        introductionElement.textContent = data[charaID - 1].introduction;
+
+        ageElement.textContent = data[charaID - 1].age;
+        sexElement.textContent = data[charaID - 1].sex;
+        heightElement.textContent = data[charaID - 1].height;
+        jobElement.textContent = data[charaID - 1].job;
+        birthdayElement.textContent = data[charaID - 1].birthday;
+        charaColorElement.textContent = data[charaID - 1].charaColor;
+
+        scenarioListElement.textContent = data[charaID - 1].scenarioList;
+        charaLinkElement.textContent = data[charaID - 1].charaLink;
+        freeCommentElement.textContent = data[charaID - 1].freeComment;
 
     });
-
-console.log("script");
