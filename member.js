16bb900e9f71ff1566148cb9ef5789dc33df357b
renaruna <79509944+renaruna@users.noreply.github.com>
2024-09-14 17:19:04 +0900

    メンバーページ追加

---
+++ member.js
@@ -0,0 +1,28 @@
+fetch('https://script.google.com/macros/s/AKfycbyjMTeRaNtT2HKEKkYdilkQg3RYP9JOlDJ8s27e7HK6NeRv67cwv03RLVyWRwDC-pN03A/exec?member=' + 'manager')
+    .then(response => response.json())
+    .then(function (data) {
+        const characterElement = document.getElementById('member-character');
+
+        let characterHtml = "";
+        for (let i = 0; i < data.length; i++) { // i=0のとき charaID=1
+            if (data[i].filename == "") {
+                continue;
+            }
+            characterHtml += '<a href="' + data[i].filename + '">';
+            characterHtml += '<div class="charaImg">';
+            characterHtml += '<img src="' + data[i].icon + '">';
+            characterHtml += '<p>' + data[i].charaName + '</p>';
+            characterHtml += '</div></a>';
+        }
+        characterElement.innerHTML = characterHtml;
+
+    });
+
+function change(text, a, b) {
+    let i = 0;
+    let length = text.length;
+    for (i = 0; i < length; i++) {
+        text = text.replace(a, b);
+    }
+    return text;
+}
