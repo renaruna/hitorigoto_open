da27199bbbbddd4cbc08702ab6684c8402f5f1c8
renaruna <79509944+renaruna@users.noreply.github.com>
2024-10-20 21:15:39 +0900

    メニューボタンを一からやってみる

--- common.js
+++ common.js
@@ -1,37 +1,22 @@
 $(function () {
     var userAgent = navigator.userAgent; // ユーザーエージェント判定
 
-    //menu右から表示
+    /* menuボタン押して、navigator全画面表示 */
     var $nav = $('.navigator');
 
-    $('.front').click(function () {
-        console.log("押された");
+    console.log("読み込まれた");
 
-        $('.front').fadeToggle(250);//250ミリ秒で非表示
-        $('.back').fadeToggle(250);//250ミリ秒で表示
+    // $('.front').click(function () {
+    //     console.log("押された");
 
-        $nav.animate({ bottom: 0 }, 500);
-    });
+    //     $('.front').fadeToggle(250);//250ミリ秒で非表示
+    //     $('.back').fadeToggle(250);//250ミリ秒で表示
 
-    //アイコンを押しても戻る
-    $('.back').click(function () {
-        $('.back').fadeToggle(250);//250ミリ秒で非表示
-        $('.front').fadeToggle(250);//250ミリ秒で表示
-    });
+    //     $nav.animate({ bottom: 0 }, 500);
+    // });
 
-    //メニューを押しても戻る
-    $nav.click(function () {
-        $('.back').fadeToggle(250);//250ミリ秒で非表示
-        $('.front').fadeToggle(250);//250ミリ秒で表示
-    });
 
-    //メニューのリンクを押しても戻る
-    $('.menu a[href*="#"]').click(function () {
-        $('.back').fadeToggle(250);//250ミリ秒で非表示
-        $('.front').fadeToggle(250);//250ミリ秒で表示
-    });
-
-    //ページ内リンクへスクロール
+    /* ページ内リンクへスクロール */
     $('a[href*="#"]').click(function () { //全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK。元は#page-link a[href*="#"]。
         var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
         var pos = $(elmHash).offset().top;	//idの上部の距離を取得
@@ -40,11 +25,11 @@ $(function () {
     });
 
 
-    //トップへ戻るボタン
+    /* トップへ戻るボタン実装 */
     var topBtn = $('#page_top');
     topBtn.hide();
 
-    //ボタンの表示設定
+    //ある程度スクロールしたら、ボタン表示する
     $(window).scroll(function () {
         if ($(this).scrollTop() > 200) {
             // 画面を200pxスクロールしたら、800msかけてボタンを表示
