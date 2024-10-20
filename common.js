b2098028783b61ff26b0bf06d60a2498980a6a55
renaruna <79509944+renaruna@users.noreply.github.com>
2024-10-21 00:36:41 +0900

    トップへ戻るボタンの表示をどうにかし隊

--- common.js
+++ common.js
@@ -15,27 +15,27 @@ $(function () {
         $('.hamburger-menu').toggleClass('hamburger-menu--open');
     });
 
-    /* ページ内リンクへスクロール */
-    $('a[href*="#"]').click(function () { //全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK。元は#page-link a[href*="#"]。
-        var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-        var pos = $(elmHash).offset().top;	//idの上部の距離を取得
-        $('body,html').animate({ scrollTop: pos }, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
-        return false;
-    });
+    // /* ページ内リンクへスクロール */
+    // $('a[href*="#"]').click(function () { //全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK。元は#page-link a[href*="#"]。
+    //     var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+    //     var pos = $(elmHash).offset().top;	//idの上部の距離を取得
+    //     $('body,html').animate({ scrollTop: pos }, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+    //     return false;
+    // });
 
 
     /* トップへ戻るボタン実装 */
     var topBtn = $('#page_top');
     topBtn.hide();
 
-    //ある程度スクロールしたら、ボタン表示する
+    // ある程度スクロールしたら、ボタン表示する
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            // 画面を200pxスクロールしたら、800msかけてボタンを表示
-            topBtn.fadeIn(800);
+        if ($(this).scrollTop() > 500) {
+            // 画面を500pxスクロールしたら、800msかけてボタンをslide upで表示
+            topBtn.slideUp(800);
         } else {
-            // 画面が200pxより上なら、800msかけてボタンを非表示
-            topBtn.fadeOut(800);
+            // 画面が500pxより上なら、800msかけてボタンをslide downで非表示
+            topBtn.slideDown(800);
         }
     });
 
