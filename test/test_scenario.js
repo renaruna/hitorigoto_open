cb04e66e5683a668223ac145ce2995a75435551e
renaruna <79509944+renaruna@users.noreply.github.com>
2024-10-26 20:10:52 +0900

    Update test_scenario.js

--- test/test_scenario.js
+++ test/test_scenario.js
@@ -1,4 +1,4 @@
-fetch('https://script.google.com/macros/s/AKfycbyoDRrmUAPJkOB6_g81A5exR0aWSEl6wI3FWzmmX4tMNzIHc_x2QLsbU-jrTrnQBULP/exec')
+fetch('https://script.google.com/macros/s/AKfycbzjCXoB0CWk9irqnG4hBaGSHA9EPDefmMzAx4mXrB9XoR0R78LQLUjXT_xSy1bpMcjahg/exec')
       .then(response => response.json())
       .then(data => {
         let jsElement = document.getElementById('js-content');
