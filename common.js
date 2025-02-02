f34e42dc161f0e4265034002d6da8adf3ecd6ca0
renaruna <79509944+renaruna@users.noreply.github.com>
2025-02-03 04:15:51 +0900

    Update common.js
    ページ内リンクへスクロールするときのアニメーションを復活

--- common.js
+++ common.js
@@ -15,14 +15,13 @@ $(function () {
         $('.hamburger-menu').toggleClass('hamburger-menu--open');
     });
 
-    // /* ページ内リンクへスクロール */
-    // $('a[href*="#"]').click(function () { //全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK。元は#page-link a[href*="#"]。
-    //     var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-    //     var pos = $(elmHash).offset().top;	//idの上部の距離を取得
-    //     $('body,html').animate({ scrollTop: pos }, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
-    //     return false;
-    // });
-
+    /* ページ内リンクへスクロール */
+    $('a[href*="#"]').click(function () { //全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK。元は#page-link a[href*="#"]。
+        var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+        var pos = $(elmHash).offset().top;	//idの上部の距離を取得
+        $('body,html').animate({ scrollTop: pos }, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+        return false;
+    });
 
     /* トップへ戻るボタン実装 */
     var topBtn = $('#page_top');
