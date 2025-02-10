abfc8ecea8a0bd42d9a99737a85dfa0637660b1f
renaruna <79509944+renaruna@users.noreply.github.com>
2025-02-10 13:21:09 +0900

    v2

---
+++ member/test_v2_member.js
@@ -0,0 +1,56 @@
+fetch('https://script.google.com/macros/s/AKfycbwa2cetqBvNrHkmr-i8gaiMUC12uDHoAG4RI7XYIzyHh4UW_joqJTGSp5fQqENEzjCq/exec'
+    + '?member=manager&charaID=off&scenarioID=off')
+    .then(response => response.json())
+    .then(function (data) {
+        const charaArray = data.chara;
+        const kpArray = data.kp;
+        const plArray = data.pl;
+
+        const characterElement = document.getElementById('member-character');
+        const kpElement = document.getElementById('member-kp');
+        const plElement = document.getElementById('member-pl');
+
+        //キャラ一覧
+        let characterHtml = '';
+        let existCharaNum = charaArray.length;
+        for (let i = 0; i < existCharaNum; i++) {
+            characterHtml += '<a href="' + charaArray[i].charaFilename + '">';
+            characterHtml += '<div class="charaImg">';
+            characterHtml += '<img src="' + charaArray[i].charaIcon + '">';
+            characterHtml += '<p>' + charaArray[i].charaName + '</p>';
+            characterHtml += '</div></a>';
+        }
+
+        // 探索者たちがflexで並べられている→最後の行のボックスたちを左寄せにしたい
+        // →空白のボックスを用意する
+        blankNum = 6 - (existCharaNum % 6);
+        for (let i = 0; i < blankNum; i++) {
+            characterHtml += '<a style="width:123px;"></a>';
+        }
+
+        characterElement.innerHTML = characterHtml;
+
+        //KP一覧
+        let kpHtml = '';
+        for (let i = 0; i < kpArray.length; i++) {
+            kpHtml += '<a href="../../scenario/' + kpArray[i].scenarioFilename + '">'
+                + kpArray[i].scenarioName + '</a><br>';
+        }
+        //最後の改行文字を消して埋め込み
+        kpElement.innerHTML = kpHtml.slice(0, -4);
+
+        //PL一覧
+        let plHtml = '';
+        for (let i = 0; i < plArray.length; i++) {
+            //シナリオ
+            plHtml += '<p><a href="../../scenario/' + plArray[i].scenarioFilename + '">'
+                + plArray[i].scenarioName + '</a>';
+            //：【HO】
+            plHtml += '：' + plArray[i].hoSentence;
+            //キャラ
+            plHtml += '<a href="' + plArray[i].charaFilename + '">'
+                + plArray[i].charaName + '</a></p>';
+        }
+        plElement.innerHTML = plHtml;
+
+    });
