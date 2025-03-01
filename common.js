576ed862ec7a8f927992eed0018c153e2ed433b6
renaruna <79509944+renaruna@users.noreply.github.com>
2025-03-02 07:50:13 +0900

    アコーディオン実装

--- common.js
+++ common.js
@@ -47,4 +47,49 @@ $(function () {
         return false;
     });
 
+
+    /* アコーディオン実装
+        参考：https://lab.dxo.co.jp/notes/web-design/accordion-menu */
+    $(document).ready(function () {
+        let accordionDetails = '.js-accordion';
+        let accordionSummary = '.js-accordion-title';
+        let accordionContent = '.js-accordion-content';
+        let speed = 500;
+
+        $(accordionSummary).each(function () {
+            $(this).on("click", function (event) {
+                // summaryにis-activeクラスを切り替え
+                $(this).toggleClass("is-active");
+                // デフォルトの挙動を無効化
+                event.preventDefault();
+                if ($(this).parent($(accordionDetails)).attr("open")) {
+                    // アコーディオンを閉じるときの処理
+                    $(this).nextAll($(accordionContent)).slideUp(speed, function () {
+                        // アニメーションの完了後にopen属性を取り除く
+                        $(this).parent($(accordionDetails)).removeAttr("open");
+                        // display:none;を消して、ページ内検索にヒットするようにする
+                        $(this).show();
+                    });
+                } else {
+                    // アコーディオンを開くときの処理
+                    // open属性を付ける
+                    $(this).parent($(accordionDetails)).attr("open", "true");
+                    // いったんdisplay:none;してからslideDownで開く
+                    $(this).nextAll($(accordionContent)).hide().slideDown(speed);
+                }
+            });
+
+            $(this).closest(accordionDetails).find('.close-btn').on('click', function (eventclosee) {
+                eventclosee.preventDefault();
+                //「閉じる」ボタンがクリックされた場合にsummaryの.is-activeを外す
+                $(this).closest(accordionDetails).find(accordionSummary).removeClass("is-active");
+                //「閉じる」ボタンがクリックされた場合にdetails-contentを閉じる
+                $(this).closest(accordionContent).slideUp(speed, function () {
+                    //「閉じる」ボタンがクリックされた場合にdetailsのopen属性を外す
+                    $(this).closest(accordionDetails).removeAttr("open");
+                });
+            });
+        });
+    });
+
 });
